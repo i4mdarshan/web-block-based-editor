68e01d72c65e0784dfe3ebe2d724cd71c7124b86
Darshan Mahajan <darshan@dotminds.in>
2024-05-09 11:40:57 +0530

    Configure SignIn and SignUp forms layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./globals.css";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import { Home } from "./_root/pages";
 const App = () => {
   return (
     <main className='flex h-screen'>
-      <h1>Hello World</h1>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SignInForm />}></Route>
+          <Route path='/sign-up' element={<SignUpForm />}></Route>
+        </Route>
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
     </main>
   );
 };
